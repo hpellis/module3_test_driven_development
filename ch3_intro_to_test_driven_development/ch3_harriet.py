6cc333944f345bc2bdc751a125ac0c6baa647f8f
hpellis <42347904+hpellis@users.noreply.github.com>
2019-01-29 10:23:15 +0100

    initial commit

---
+++ ch3_intro_to_test_driven_development/ch3_harriet.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 24 09:02:21 2019
+
+@author: 612383249
+"""
+
+#----------------------------------------------------------------------------------------------------------------------
+# CHAPTER 3 - INTRODUCTION TO TEST-DRIVEN DEVELOPMENT
+#----------------------------------------------------------------------------------------------------------------------
+
+#test-driven development is the process of implementing code by writing tests first and then writing code that passes the tests
+#TDD is a cycle of writing tests, followed by coding and repeating
+#the code goes through many iterations of tests, writing and development
+#testing ensures correctness under a basic set of conditions
+#syntax errors will almost certaily be caught by running tests
+#it is not about proving the code is correct under any set of conditions, rather aiming for a reasonably complete set of possible conditions
+#testing requires you to consider how the code will function in unusual conditions, and may reveal logical errors in the code
+
+#testing is helpful when refactoring, or updating code
+#unit testing ensures that changes to your code don't break other components
+
+#code designed with testing in mind is generally modular, as this enables testing
+
+###implementing TDD
+#1. write a test
+#2. write the minimum code to pass the test
+#3. write another test
+#4. adapt and extend current code
+#5. repeat until you can write no more tests
+
+#repeat this process for every feature
+
+#rule 1: the goal of the code you write is to pass the test
+#rule 2: you only write the code you need to make that one test pass
+
+
+
+
+
+
+
+
+
