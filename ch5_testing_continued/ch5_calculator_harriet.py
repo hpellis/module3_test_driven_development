5d1fdbd6cfacb8988c661e6ed822d88e91ff87ce
hpellis <harriet.p.ellis@gmail.com>
2019-02-05 16:06:26 +0000

    updating chapter 5 files

--- ch5_testing_continued/ch5_calculator_harriet.py
+++ ch5_testing_continued/ch5_calculator_harriet.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 30 09:58:29 2019
+Created on Wed Jan 30 10:06:37 2019
 
-@author: 612383249
+@author: harri
 """
 
+#--------------------------------------------------------------------------------------------------------------
+# CHAPTER 5 - EXTERNAL TESTING TOOL
+#----------------------------------------------------------------------------------------
+
+
+# TASK 1  - WRITE FUNCTION FILE
+#------------------------------------------------------------------------------------------
+
+class Calculator():
+    def add (self, a, b):
+        number_types = (int, float, complex)
+        if isinstance (a, number_types) and isinstance (b, number_types):
+            return a + b
+        else:
+            raise ValueError
--- ch5_testing_continued/ch5_harriet.py
+++ ch5_testing_continued/ch5_harriet.py
@@ -1,14 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 30 09:57:35 2019
+Created on Wed Jan 30 10:05:49 2019
 
-@author: 612383249
+@author: harri
 """
 
-#----------------------------------------------------------------------------------------------------------------------
-# CHAPTER 5 - TESTING CONT
-#----------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------
+# CHAPTER 5 - EXTERNAL TESTING TOOL
+#----------------------------------------------------------------------------------------
 
+#the main difference between unittest, pytest and nose is the amount of information they return
+#pytest can tell you which variables inside a function fail
+#nose can do a similar thing
 
-# TASK 1 - 
-#--------------------------------------------------------------------------------------
+import unittest
+from ch5_calculator_harriet import Calculator
+
+# TASK 1 - WRITE TEST FILE
+#------------------------------------------------------------------------------------------
+
+class CalculatorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.calc = Calculator()
+    
+    def test_add(self):
+        self.assertEqual(self.calc.add(2,2), 4)
+        self.assertEqual(self.calc.add(-2, 2), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        
+    def test_args_numbers(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 'three')
+        self.assertRaises(ValueError, self.calc.add, 'two', 2)
+        self.assertRaises(ValueError, self.calc.add, 2, 'three')
+    
+        
+if __name__ == 'main':
+    unittest.main()
+        
+        
+        
+        
+        
