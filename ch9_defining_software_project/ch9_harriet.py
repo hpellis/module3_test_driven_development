88dd3a23e62bfb571542265fbc0cf3b561dc5d12
hpellis <harriet.p.ellis@gmail.com>
2019-02-06 10:22:08 +0000

    update to chapter 9

---
+++ ch9_defining_software_project/ch9_harriet.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  6 09:16:04 2019
+
+@author: harriet
+"""
+
+#----------------------------------------------------------------------------------------------------------------------
+# CHAPTER 9 - DEFINING A SOFTWARE PROJECT
+#----------------------------------------------------------------------------------------------------------------------
+
+#the goal of software project management is to deliver a system with the required functionality (scope), on time (schedule) within budget and with adequate quality
+
+#Ideation
+#Requirements gathering
+#Development
+#Testing
+#Deployment
+
+#Waterfall vs Agile methodologies
+
+#MOSCOW prioritisation rule
+#Must Have, Should Have, Could Have, Won't Have
+
+#Phone Book Project MOSCOW 
+
+#Must Have
+#Ability to search database for businesses by business name and business type
+#Ability to search database for people by person last name
+#User interface to conduct search
+#Display of results to user
+
+#Should Have
+#Ability to use additional search filter for location
+#Ability to sort results alphabetically or by location
+
+#Could Have
+#Limit the amount of results returned
+#Ability to search database for similar search queries
+
+#Won't Have
+#Further interactions with search results (gifts, mapping, website links etc)
